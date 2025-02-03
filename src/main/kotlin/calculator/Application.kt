6d4917e8a626fb92cd642d06371dface1205511c
choi-day <choisschoco@gmail.com>
2025-02-04 01:35:42 +0900

    feat: 구분자를 인식하는 함수와 합하는 함수 구현
    Co-authored-by: Hyemin <143622050+hyeminililo@users.noreply.github.com>

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -2,11 +2,31 @@ package calculator
 
 fun main() {
     print("덧셈할 문자열을 입력해 주세요.")
-    var num = readLine()
+    var num = readLine().toString()
+    if(identifySign==true){
 
+    }
+    else {
+        val list1 = mutableListOf<Int>()
+        list1 = basicSplit(num)
+    }
+
+    sum()
 }
 
 fun basicSplit(a : String) : List<Int>{
-return a.split(',',':')
+    return a.split(',',':')
 }
 
+fun identifySign(a : String) : Boolean {
+    if(a.first=="/")    
+     return true
+}
+
+fun sum(a : List<Int>) : Int {
+    var result = 0
+    for(i in a) {
+        result += i
+    }
+    return result
+}
