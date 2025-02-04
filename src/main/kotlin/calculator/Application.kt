afe3561596d760cb79f9c7ea4f12ef260439d372
choi-day <choisschoco@gmail.com>
2025-02-05 02:06:16 +0900

    feat: 예외 처리 기능 함수 구현
    Co-authored-by: Hyemin <hyeminililo@users.noreply.github.com>

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -3,6 +3,7 @@ package calculator
 fun main() {
     print("덧셈할 문자열을 입력해 주세요.")
     var num = readLine().toString()
+
     if(num == " " || num == ""){
         zeroReturn()
     }
@@ -12,6 +13,14 @@ fun zeroReturn():Int{
     return 0;
 }
 
+fun identifySign(a : String) : Boolean {
+    val slash = a.first()
+    if(slash.equals("/")){
+    return true
+    }
+    return false
+}
+
 fun basicSplit(a : String) : List<String>{
     return a.split(',',':')
 }
@@ -19,14 +28,6 @@ fun basicSplit(a : String) : List<String>{
 fun customSplit(a : String, sign : Char) : List<String>{
     val b = a.slice(5..a.length)
     return b.split(sign)
-}
-
-fun identifySign(a : String) : Boolean {
-    val slash = a.first()
-    if(slash.equals("/")){
-    return true
-    }
-    return false
 }
 
 fun extractSign(a : String) : String {
@@ -51,3 +52,22 @@ fun sum(a : List<Int>) : Int {
     }
     return result
 }
+
+// 예외처리 부분 
+fun inputnotPositive():{
+    throw IllegalArgumentException("입력값이 양수가 아닙니다. ")
+}
+
+fun signNotInt(){
+        throw IllegalArgumentException("계산할 숫자가 따로 없습니다.")
+}
+fun incorrectSign(){
+        throw IllegalArgumentException("구분자와 양수가 아닌 문자가 입력되었습니다.")
+}
+fun undefineSign(){
+        throw IllegalArgumentException(" //와\n사이에 커스텀 구분자를 정의하지 않았습니다.")
+}
+fun customSignNotChar(){
+        throw IllegalArgumentException("커스텀 구분자가 문자가 아닙니다.")
+}
+  
