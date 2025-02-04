0c308239336083cc07052b76637708284a886b16
choi-day <choisschoco@gmail.com>
2025-02-05 01:09:44 +0900

    feat: 입력값이 없을 때 0으로 반환하는 함수 구현
    Co-authored-by: Hyemin <143622050+hyeminililo@users.noreply.github.com>

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -3,6 +3,9 @@ package calculator
 fun main() {
     print("덧셈할 문자열을 입력해 주세요.")
     var num = readLine().toString()
+    if(num == " " || num == ""){
+        zeroReturn()
+    }
 }
 
 fun basicSplit(a : String) : List<String>{
@@ -12,6 +15,10 @@ fun basicSplit(a : String) : List<String>{
 fun customSplit(a : String, sign : Char) : List<String>{
     var b = a.slice(5..a.length)
     return a.split(sign)
+}
+
+fun zeroReturn():Int{
+    return 0;
 }
 
 fun identifySign(a : String) : Boolean {
